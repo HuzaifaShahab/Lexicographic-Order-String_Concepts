c3bfe994aa9a7315e54f364d370ca07d4ed52360
HUZAIFA SHAHAB <huzaifarifatullah@gmail.com>
2023-08-06 22:03:11 +0500

    Update largestString.java

--- src/largestString.java
+++ src/largestString.java
@@ -4,6 +4,7 @@ public class largestString {
 
         String largest = Cars[0] ;                                   // Assumption
         for (int i=1 ; i< Cars.length ; i++){
+            // largest < 0 -- Negative m/ l<C[i]
            if(largest.compareTo(Cars[i]) < 0)        // On the basis of Lexicographic-Order
                largest = Cars[i] ;
         }
