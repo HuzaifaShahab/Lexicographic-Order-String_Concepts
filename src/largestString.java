7d71d5220180e858284585bfd88af23fcc11a1a4
HUZAIFA SHAHAB <huzaifarifatullah@gmail.com>
2023-08-06 21:41:03 +0500

    Update largestString.java

--- src/largestString.java
+++ src/largestString.java
@@ -1,5 +1,12 @@
 public class largestString {
     public static void main(String[] args) {
-        
+        String [] Cars = {"Corolla" , "Civic" , "Prius" , "Honda"} ;
+
+        String largest = Cars[0] ;
+        for (int i=1 ; i< Cars.length ; i++){
+           if(largest.compareTo(Cars[i]) < 0)        // Orders
+               largest = Cars[i] ;
+        }
+        System.out.println("Largest String Among Them Is : "+largest);
     }
 }
