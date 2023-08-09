688ff3177d66665868edcb89d2d5781366ff0be6
HUZAIFA SHAHAB <huzaifarifatullah@gmail.com>
2023-08-09 23:17:31 +0500

    Update largestString.java

--- src/largestString.java
+++ src/largestString.java
@@ -1,11 +1,11 @@
 public class largestString {
     public static void main(String[] args) {
-        String [] Cars = {"Corolla" , "Civic" , "Prius" , "Honda"} ;  // Collection of Cars-Strings
+        String [] Cars = {"Corolla" , "Civic" , "Prius" , "Honda"} ;  // Collection of Cars-Strings.
 
-        String largest = Cars[0] ;                                   // Assumption
+        String largest = Cars[0] ;                                   // Assumption for largest string.
         for (int i=1 ; i< Cars.length ; i++){
             // largest < 0 -- Negative m/ l<C[i]
-           if(largest.compareTo(Cars[i]) < 0)        // On the basis of Lexicographic-Order
+           if(largest.compareTo(Cars[i]) < 0)        // On the basis of Lexicographic-Order.
                largest = Cars[i] ;
         }
         System.out.println("Largest String Among Them Is : "+largest);
